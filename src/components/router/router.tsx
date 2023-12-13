39ee8a52db01ceb27e57e3fb5f067121ae179db9
Renata Giera-Dubiel <renatagiera@gmail.com>
2023-12-13 15:08:57 +0000

    fix: add routers

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,2 +1,19 @@
-export {}
-// what goes here??
+import { Routes, Route } from "react-router-dom";
+import Home from "../home/home";
+import About from "../about/about";
+import Products from "../products/products";
+import Help from "../help/help";
+import MainLayout from "../layouts/main_layouts";
+
+const Router = () => (
+  
+  <Routes>
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/help" element={<Help />} />
+    </Route>
+  </Routes>
+)
+export default Router;
