3e3b141768186b7144007e3208d63bbe92aaf5e2
Renata Giera-Dubiel <renatagiera@gmail.com>
2023-12-13 17:02:25 +0000

    fix: add routes to product nav

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -4,15 +4,19 @@ import About from "../about/about";
 import Products from "../products/products";
 import Help from "../help/help";
 import MainLayout from "../layouts/main_layouts";
+import Product from "../products/product/product";
+import NotFound from "../not_found/not_found";
 
 const Router = () => (
   
   <Routes>
     <Route path="/" element={<MainLayout />}>
-      <Route index element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/help" element={<Help />} />
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+            <Route path="/products/:productId" element={<Product />}/>
+        <Route path="/help" element={<Help />} />
+        <Route path="*" element={<NotFound />} />
     </Route>
   </Routes>
 )
